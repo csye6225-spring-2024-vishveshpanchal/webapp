88e0b591ef1695b3e2453c318b396e82c2cde924
vishveshpanchal <panchal.vis@northeastern.edu>
2024-02-16 15:12:54 -0500

    trying to run API Test workflows 9

--- config/config.js
+++ config/config.js
@@ -10,12 +10,12 @@ module.exports = {
     "port": process.env.DB_PORT
   },
   "test": {
-    "username": process.env.DB_USERNAME_TEST,
-    "password": process.env.DB_PASSWORD_TEST,
-    "database": process.env.DB_NAME_TEST,
-    "host": process.env.DB_HOST_TEST,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
-    "port": process.env.DB_PORT_TEST
+    "port": process.env.DB_PORT
   },
   "production": {
     "username": process.env.DB_USERNAME,
